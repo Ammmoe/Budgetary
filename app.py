31d3c52040ea12099b71cbe3e09be45cc868a22f
Ammmoe <ag.mmoe@gmail.com>
2024-07-30 03:53:34 +0700

    add change password function and edit error handling elements in login, register, and change password pages.

--- app.py
+++ app.py
@@ -1549,7 +1549,7 @@ def history():
             )
 
             return render_template("history.html", income_spending_rows=income_spending_rows, investment_rows=investment_rows, \
-                debt_rows=debt_rows, currency=currency)
+                debt_rows=debt_rows, currency=currency, start_date=start_date, end_date=end_date)
 
         else:
             income_spending_query = income_spending_query.format(income_where_clause=income_where_clause, spending_where_clause=spending_where_clause)
@@ -1575,19 +1575,27 @@ def history():
 def login():
     """Log user in"""
 
+    flash_messages = session.get('_flashes', [])
+
     # Forget any user_id
     session.clear()
+
+    # Restore flash messages after clearing session
+    if flash_messages:
+        session['_flashes'] = flash_messages
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            flash('Please provide username!', 'alert-danger')
+            return redirect(url_for('login'))
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            flash('Please provide password!', 'alert-danger')
+            return redirect(url_for('login'))
 
         # Query database for username
         rows = db.execute(
@@ -1598,7 +1606,9 @@ def login():
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
-            return apology("invalid username and/or password", 403)
+            flash('Invalid username or password!', 'alert-danger')
+            # Clear the session but keep flash messages
+            return redirect(url_for('login'))
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -1626,43 +1636,103 @@ def logout():
 def register():
     """Register user"""
 
+    flash_messages = session.get('_flashes', [])
+
     # Forget any user_id
     session.clear()
+
+    # Restore flash messages after clearing session
+    if flash_messages:
+        session['_flashes'] = flash_messages
 
     # Get registered user data via POST
     if request.method == "POST":
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 400)
+            flash('Please provide username!', 'alert-danger')
+            return redirect(url_for('register'))
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 400)
+            flash('Please provide password!', 'alert-danger')
+            return redirect(url_for('register'))
 
         # Ensure re-type password field was submitted
         elif not request.form.get("confirmation"):
-            return apology("must confirm password", 400)
+            flash('Please confirm password!', 'alert-danger')
+            return redirect(url_for('register'))
 
         # Ensure password and re-typed password match
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("passwords do not match", 400)
+            flash('Passwords do not match!', 'alert-danger')
+            return redirect(url_for('register'))
 
         # Register username and hashed password in the database
         try:
-            rows = db.execute(
+            db.execute(
                 "INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get(
                     "username"), generate_password_hash(request.form.get("password"))
             )
             # Redirect user to home page
             return redirect("/login")
 
-        except ValueError:
-            return apology("username already exists", 400)
+        except:
+            flash('Username already exists!', 'alert-danger')
+            return redirect(url_for('register'))
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
+    
+
+@app.route("/change-password", methods=["GET", "POST"])
+def change_password():
+    # Get registered user data via POST
+    if request.method == "POST":
+        user_id = session.get("user_id")
+        old_password = request.form.get("old-password")
+        new_password = request.form.get("new-password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not old_password:
+            flash('Please input old password!', 'alert-danger')
+            return redirect(url_for('change_password'))
+
+        # Ensure password was submitted
+        if not new_password:
+            flash('Please input new password!', 'alert-danger')
+            return redirect(url_for('change_password'))
+
+        # Ensure re-type password field was submitted
+        if not confirmation:
+            flash('Please confirm new password!', 'alert-danger')
+            return redirect(url_for('change_password'))
+
+        # Ensure password and re-typed password match
+        if new_password != confirmation:
+            flash('New passwords do not match!', 'alert-danger')
+            return redirect(url_for('change_password'))
+        
+        old_password_hash = db.execute("SELECT hash FROM users WHERE id = ?", user_id)
+        print(f"old_password_hash: {old_password_hash}")
+
+        # Check if old password is correct
+        if not check_password_hash(old_password_hash[0]['hash'], old_password):
+            flash('Wrong old password!', 'alert-danger')
+            return redirect(url_for('change_password'))
+        
+        elif check_password_hash(old_password_hash[0]['hash'], old_password):
+            db.execute(
+                "UPDATE users SET hash = ? WHERE id = ?", generate_password_hash(new_password), user_id
+            )
+            flash('Password changed successfully!', 'alert-success')
+            return redirect(url_for('change_password'))
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("change-password.html")
     
 
 # Convert usd values into other currencies
